3cfeae818f635ca397dbe0bc17e2d336c05280a0
HuangYuhui <Gentleman_0109@outlook.com>
2020-12-19 15:14:47 +0800

    ✨ add the new feature of face and eye detection

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,7 +1,7 @@
 '''
 Author: GoogTech
 Date: 2020-12-18 19:44:25
-LastEditTime: 2020-12-19 12:17:02
+LastEditTime: 2020-12-19 15:09:01
 Description: 调用百度 AI 的一个简易人脸识别程序
 Version: 0.0.1
 '''
@@ -23,19 +23,46 @@ client = AipFace(APP_ID, API_KEY, SECRET_KEY)
 IMAGE_TYPE = 'BASE64'
 # 用户组
 GROUP = '01'
+# 摄像头
+cap = cv2.VideoCapture(0)
+# 人脸分类级联
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +
+                                     'haarcascade_frontalface_default.xml')
+# 人眼分类级联
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +
+                                    'haarcascade_eye.xml')
 
 
 # 照相函数
 def getimage():
-    cap = cv2.VideoCapture(0)
-    while 1:
+    # 持续运行, 点击 Esc 键退出
+    while (True):
+        # 获取摄像头拍摄到的画面
         ret, frame = cap.read()
-        cv2.imshow('Camera Frame', frame)
+        faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+        img = frame
+        # 用人脸级联分类器引擎在人脸区域进行脸部识别
+        for (x, y, w, h) in faces:
+            # 画出人脸框, 蓝色, 画笔宽度微
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            # 框选出人脸区域, 在人脸区域而不是全图中进行人眼检测, 节省计算资源
+            face_area = img[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(face_area, 1.3, 15)
+            # 用人眼级联分类器引擎在人脸区域进行人眼识别返回的 eyes 为眼睛坐标列表
+            for (ex, ey, ew, eh) in eyes:
+                #画出人眼框, 绿色, 画笔宽度为 1
+                cv2.rectangle(face_area, (ex, ey), (ex + ew, ey + eh),
+                              (0, 255, 0), 1)
+
+        # 实时展示效果画面
+        cv2.imshow('Camera Frame', img)
+        # 每 1 毫秒监听一次键盘动作
         flag = cv2.waitKey(1)
         if flag == 13:  # 按下回车键进行拍照
             output_path = os.path.join(
                 "getCamera.jpg")  # 默认情况下 os.path.join 的路径为当前路径
-            cv2.imwrite(output_path, frame)
+            # cv2.imwrite(output_path, frame)
+            cv2.imwrite(output_path, img)
             voicePrompt("已拍照, 请按 Esc 键退出拍照进入人脸验证环节 !")
             time.sleep(2)
         if flag == 27:  # 按下ESC键退出拍照
