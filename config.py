2dc29fba1f55663786481830a7450063c7828764
HuangYuhui <Gentleman_0109@outlook.com>
2020-12-30 16:51:42 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+'''
+Author: GoogTech
+Date: 2020-12-30 16:42:42
+LastEditTime: 2020-12-30 16:43:31
+Description: Configuration INFO
+'''
+
+# 百度语音 API 配置参数
+APP_ID = ""
+API_KEY = ""
+SECRET_KEY = ""
